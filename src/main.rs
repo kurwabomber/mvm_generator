6b3bec4365abaa56120b8514c25b5d178cbcc4b9
Norad <davidWnguyen@proton.me>
2023-05-18 18:19:13 -0400

    tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    map: String,
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long)]
+    start_money: i32,
+    #[arg(short, long)]
+    waves: i32,
+    #[arg(short, long)]
+    respawnwavetime: i32,
+    #[arg(short, long)]
+    config: String
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
