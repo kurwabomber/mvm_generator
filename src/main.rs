d111dfdf5a5e3ce9a764daad1b89888b20eb2bc7
Norad <davidWnguyen@proton.me>
2023-05-27 13:08:08 -0400

    mission info

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs, path};
+use std::fs;
 use clap::Parser;
+use serde_json::Value;
 mod mission;
 use crate::mission::Mission;
 
@@ -22,11 +23,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         let map_config = fs::read_to_string("./config/maps.json")?;
         let map: serde_json::Value = serde_json::from_str(&map_config)?;
-        let map_info = map.get(&args.map).unwrap();
+        let map_info = map.get(&args.map).unwrap().clone();
 
-        let path_length = map_info.get("bot_path_length");
-        if path_length != None{
-            mission.bot_path_length = path_length.unwrap().as_f64().unwrap();
+        //does anyone know a better way to do this...
+        if let Some(path_length) = map_info.get("bot_path_length") {
+            mission.bot_path_length = path_length.as_f64().unwrap();
+        }
+        if let Some(spawn_bot_areas) = map_info.get("spawnbots") {
+            mission.spawn_bot_areas = spawn_bot_areas.as_array().unwrap().iter().map(|x| x.as_str().unwrap().to_owned()).collect();
+        }
+        if let Some(spawn_giants_areas) = map_info.get("spawngiants") {
+            mission.spawn_giants_areas = spawn_giants_areas.as_array().unwrap().iter().map(|x| x.as_str().unwrap().to_owned()).collect();
+        }
+        if let Some(spawn_boss_areas) = map_info.get("spawnbosses") {
+            mission.spawn_boss_areas = spawn_boss_areas.as_array().unwrap().iter().map(|x| x.as_str().unwrap().to_owned()).collect();
+        }
+        if let Some(spawn_tank_areas) = map_info.get("spawntanks") {
+            mission.spawn_tank_areas = spawn_tank_areas.as_array().unwrap().iter().map(|x| x.as_str().unwrap().to_owned()).collect();
+        }
+        if let Some(max_tank_speed) = map_info.get("max_tank_speed") {
+            mission.max_tank_speed = max_tank_speed.as_f64().unwrap();
+        }
+        if let Some(engineers_enabled) = map_info.get("engies") {
+            mission.engineers_enabled = engineers_enabled.as_bool().unwrap();
         }
     }
     {
@@ -34,6 +53,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     {
         let mission_config = fs::read_to_string("./config/".to_string() + &args.config)?;
+        let mission_info: serde_json::Value = serde_json::from_str(&mission_config)?;
+
+        if let Some(wave_amount) = mission_info.get("wave_amount") {
+            mission.wave_amount = wave_amount.as_i64().unwrap();
+        }
+        if let Some(money_per_wave) = mission_info.get("money_per_wave") {
+            mission.money_per_wave = money_per_wave.as_str().unwrap().to_owned();
+        }
+        if let Some(bot_giant_chance) = mission_info.get("bot_giant_chance") {
+            mission.bot_giant_chance = bot_giant_chance.as_f64().unwrap();
+        }
+        if let Some(bot_boss_waves) = mission_info.get("bot_boss_waves") {
+            mission.bot_boss_waves = bot_boss_waves.as_i64().unwrap();
+        }
+        if let Some(bot_superboss_waves) = mission_info.get("bot_superboss_waves") {
+            mission.bot_superboss_waves = bot_superboss_waves.as_i64().unwrap();
+        }
+        if let Some(bot_damage_outgoing) = mission_info.get("bot_damage_outgoing") {
+            mission.bot_damage_outgoing = bot_damage_outgoing.as_str().unwrap().to_owned();
+        }
+        if let Some(bot_damage_incoming) = mission_info.get("bot_damage_incoming") {
+            mission.bot_damage_incoming = bot_damage_incoming.as_str().unwrap().to_owned();
+        }
+        if let Some(bot_speed_multiplier) = mission_info.get("bot_speed_multiplier") {
+            mission.bot_speed_multiplier = bot_speed_multiplier.as_str().unwrap().to_owned();
+        }
     }
 
     Ok(())
--- src/mission.rs
+++ src/mission.rs
@@ -1,22 +1,40 @@
 pub struct Mission{
+    /* General Map Info */
     pub bot_path_length: f64,
-    pub spawn_bot_areas: Vec<&'static str>,
-    pub spawn_giants_areas: Vec<&'static str>,
-    pub spawn_boss_areas: Vec<&'static str>,
-    pub spawn_tank_areas: Vec<&'static str>,
+    pub spawn_bot_areas: Vec<String>,
+    pub spawn_giants_areas: Vec<String>,
+    pub spawn_boss_areas: Vec<String>,
+    pub spawn_tank_areas: Vec<String>,
     pub max_tank_speed: f64,
     pub engineers_enabled: bool,
+    /* Mission Specific Info */
+    pub wave_amount: i64,
+    pub money_per_wave: String, //Evaluated
+    pub bot_giant_chance: f64, //0.0 -> 1.0 chance
+    pub bot_boss_waves: i64, //Every x waves has a boss.
+    pub bot_superboss_waves: i64, //Every x waves has a superboss.
+    pub bot_damage_outgoing: String, //Evaluated
+    pub bot_damage_incoming: String, //Evaluated
+    pub bot_speed_multiplier: String, //Evaluated
 }
 impl Default for Mission{
     fn default() -> Self {
         Mission { 
             bot_path_length: 0.5,
-            spawn_bot_areas: vec!["spawnbot"],
-            spawn_giants_areas: vec!["spawnbot"],
-            spawn_boss_areas: vec!["spawnbot"],
-            spawn_tank_areas: vec!["boss_path_a1"],
+            spawn_bot_areas: vec!["spawnbot".to_string()],
+            spawn_giants_areas: vec!["spawnbot".to_string()],
+            spawn_boss_areas: vec!["spawnbot".to_string()],
+            spawn_tank_areas: vec!["boss_path_a1".to_string()],
             max_tank_speed: 500.0,
             engineers_enabled: false,
+            wave_amount: 6,
+            money_per_wave: "100+50*wave".to_string(),
+            bot_giant_chance: 0.1,
+            bot_boss_waves: 3,
+            bot_superboss_waves: 5,
+            bot_damage_outgoing: "1.0".to_string(),
+            bot_damage_incoming: "1.0".to_string(),
+            bot_speed_multiplier: "1.0".to_string(),
         }
     }
 }
