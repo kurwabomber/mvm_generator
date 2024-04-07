6a99ffe8f85672f8f501ed15fca7e731cc9007bb
David Nguyen <davidWnguyen@proton.me>
2024-04-07 18:49:03 -0400

    new system fully implemented

--- src/main.rs
+++ src/main.rs
@@ -243,7 +243,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         for _squad_num in 1..mission.wavespawn_amount+1{
             if rand::thread_rng().gen::<f64>() > mission.bot_giant_chance {
-                let chosen_wavespawn = bot_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(1.0, 1000.0) ).unwrap();
+                let chosen_wavespawn = bot_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(0.0, 1000.0) ).unwrap();
                 for chosen_bot in &chosen_wavespawn.squads{
                     total_weight += chosen_bot.currency_weight;
                 }
@@ -252,7 +252,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 finalized_spawns.push(chosen_wavespawn);
             }else{
-                let chosen_wavespawn = giant_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(1.0, 1000.0) ).unwrap();
+                let chosen_wavespawn = giant_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(0.0, 1000.0) ).unwrap();
                 for chosen_bot in &chosen_wavespawn.squads{
                     total_weight += chosen_bot.currency_weight;
                 }
@@ -263,19 +263,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
         if i >= mission.bot_superboss_waves {
-            let chosen_wavespawn: &Wavespawn = superboss_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(1.0, 1000.0) ).unwrap();
+            let chosen_wavespawn: &Wavespawn = superboss_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(0.0, 1000.0) ).unwrap();
             for chosen_bot in &chosen_wavespawn.squads{
                 total_weight += chosen_bot.currency_weight;
             }
             finalized_spawns.push(chosen_wavespawn);
         }
         else if i >= mission.bot_boss_waves {
-            let chosen_wavespawn: &Wavespawn = boss_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(1.0, 1000.0) ).unwrap();
+            let chosen_wavespawn: &Wavespawn = boss_wavespawns.choose_weighted(&mut rand::thread_rng(), |item| (item.weight*(1.0-((item.rarity-wave_rarity).abs())/wave_rarity)).clamp(0.0, 1000.0) ).unwrap();
             for chosen_bot in &chosen_wavespawn.squads{
                 total_weight += chosen_bot.currency_weight;
             }
             finalized_spawns.push(chosen_wavespawn);
         }
+        
         let mut bot_id: i64 = 0;
         let mut last_bot: i64 = 0;
         for wavespawn in finalized_spawns{
